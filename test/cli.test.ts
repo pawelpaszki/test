22a9e1219865b6d59cc4e319ad9d9fcfcc7f2190
Pawel Paszki <pawelpaszki@gmail.com>
2018-03-21 20:49:58 +0000

    tt

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -7,8 +7,26 @@ describe('# CLI Test', () => {
     it('should create a container', function(done) {
       this.timeout(20000);
       const output = ChildProcessHandler.executeChildProcCommand(
-          'ts-node src/vuln-cli.ts createContainer token someimage');
-      expect(output).to.equal('Unable to authenticate token.');
+          'ts-node src/vuln-cli.ts');
+      expect(output).to.include('createContainer');
+      expect(output).to.include('startContainer');
+      expect(output).to.include('stopContainer');
+      expect(output).to.include('removeContainer');
+      expect(output).to.include('extractContainer');
+      expect(output).to.include('checkForVuln');
+      expect(output).to.include('persistVulnCheck');
+      expect(output).to.include('pullImage');
+      expect(output).to.include('removeImage');
+      expect(output).to.include('runNpmTests');
+      expect(output).to.include('updateComponents');
+      expect(output).to.include('updateComponent');
+      expect(output).to.include('updateAndReinstall');
+      expect(output).to.include('removeSrcCode');
+      expect(output).to.include('checkTag');
+      expect(output).to.include('buildImage');
+      expect(output).to.include('pushImage');
+      expect(output).to.include('register');
+      expect(output).to.include('login');
       done();
     });
   });
